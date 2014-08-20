7cabee8115f3a14cdd7702d7f5c236d9e5d3abf4
Ian Holsman <ian@holsman.com.au>
2014-08-20 10:22:24 -0500

    fix docker ref

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 export REPO=devsand00583.c034.digitalriverws.net:5000
-docker run --name dr_offline -d  -p 9000:9000 ${REPO}/dr_offline
+docker run -v /pic/cache:/pic/cache -v /pic/source:/pic/source --name dr_offline -d  -p 9000:9000 ${REPO}/dr_offline
